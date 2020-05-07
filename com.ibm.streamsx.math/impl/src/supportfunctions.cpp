cab9efb3900168e4d1db9e7cfc331d7bc0f2abcc
joergboe <joergboe@de.ibm.com>
2020-05-07 15:36:34 +0200

    Common Supportfunctions make an minimal input parameter check

--- com.ibm.streamsx.math/impl/src/supportfunctions.cpp
+++ com.ibm.streamsx.math/impl/src/supportfunctions.cpp
@@ -1,4 +1,6 @@
 #include "supportfunctions.h"
+#include <exception>
+#include <stdexcept>
 
 typedef unsigned char uchar;
 
@@ -8,6 +10,8 @@ namespace com { namespace ibm { namespace streamsx { namespace math { namespace 
  * Converts list of 8 uint8 bytes into float64
  */
 SPL::float64 uint8ListToFloat64(SPL::list<SPL::uint8> const & x) {
+	if (x.size() != 8)
+		throw std::invalid_argument("List size must be 8");
 	SPL::float64 f;
 	uchar b[] = {x[7], x[6], x[5], x[4], x[3], x[2], x[1], x[0]};
 	memcpy(&f, &b, sizeof(f));
@@ -18,6 +22,8 @@ SPL::float64 uint8ListToFloat64(SPL::list<SPL::uint8> const & x) {
  * Converts list of 4 uint8 bytes into float32
  */
 SPL::float32 uint8ListToFloat32(SPL::list<SPL::uint8> const & x) {
+	if (x.size() != 4)
+		throw std::invalid_argument("List size must be 4");
 	SPL::float32 f;
 	uchar b[] = {x[3], x[2], x[1], x[0]};
 	memcpy(&f, &b, sizeof(f));
@@ -29,6 +35,8 @@ SPL::float32 uint8ListToFloat32(SPL::list<SPL::uint8> const & x) {
  * Converts list of 8 uint8 bytes into (signed) int64
  */
 SPL::int64 uint8ListToInt64(SPL::list<SPL::uint8> const & x) {
+	if (x.size() != 8)
+		throw std::invalid_argument("List size must be 8");
 	SPL::int64 i;
 	uchar b[] = {x[7], x[6], x[5], x[4], x[3], x[2], x[1], x[0]};
 	memcpy(&i, &b, sizeof(i));
@@ -39,6 +47,8 @@ SPL::int64 uint8ListToInt64(SPL::list<SPL::uint8> const & x) {
  * Converts list of 8 uint8 bytes into (unsigned) uint64
  */
 SPL::uint64 uint8ListToUint64(SPL::list<SPL::uint8> const & x) {
+	if (x.size() != 8)
+		throw std::invalid_argument("List size must be 8");
 	SPL::uint64 i;
 	uchar b[] = {x[7], x[6], x[5], x[4], x[3], x[2], x[1], x[0]};
 	memcpy(&i, &b, sizeof(i));
@@ -50,6 +60,8 @@ SPL::uint64 uint8ListToUint64(SPL::list<SPL::uint8> const & x) {
  * Converts list of 4 uint8 bytes into (signed) int32
  */
 SPL::int32 uint8ListToInt32(SPL::list<SPL::uint8> const & x) {
+	if (x.size() != 4)
+		throw std::invalid_argument("List size must be 4");
 	SPL::int32 i;
 	uchar b[] = {x[3], x[2], x[1], x[0]};
 	memcpy(&i, &b, sizeof(i));
@@ -60,6 +72,8 @@ SPL::int32 uint8ListToInt32(SPL::list<SPL::uint8> const & x) {
  * Converts list of 4 uint8 bytes into (unsigned) uint32
  */
 SPL::uint32 uint8ListToUint32(SPL::list<SPL::uint8> const & x) {
+	if (x.size() != 4)
+		throw std::invalid_argument("List size must be 4");
 	SPL::uint32 i;
 	uchar b[] = {x[3], x[2], x[1], x[0]};
 	memcpy(&i, &b, sizeof(i));
@@ -71,6 +85,8 @@ SPL::uint32 uint8ListToUint32(SPL::list<SPL::uint8> const & x) {
  * Converts list of 2 uint8 bytes into (signed) int16
  */
 SPL::int16 uint8ListToInt16(SPL::list<SPL::uint8> const & x) {
+	if (x.size() != 2)
+		throw std::invalid_argument("List size must be 2");
 	SPL::int16 i;
 	uchar b[] = {x[1], x[0]};
 	memcpy(&i, &b, sizeof(i));
@@ -81,6 +97,8 @@ SPL::int16 uint8ListToInt16(SPL::list<SPL::uint8> const & x) {
  * Converts list of 2 uint8 bytes into (unsigned) uint16
  */
 SPL::uint16 uint8ListToUint16(SPL::list<SPL::uint8> const & x) {
+	if (x.size() != 2)
+		throw std::invalid_argument("List size must be 2");
 	SPL::uint16 i;
 	uchar b[] = {x[1], x[0]};
 	memcpy(&i, &b, sizeof(i));
